aa9cd6f7bc54df9a630bc9c393bd51844e987007
prabuwardhana <prabuwardhana03@yahoo.com>
2024-12-25 10:50:57 +0900

    change the page component name

--- inertia/pages/admin/index.tsx
+++ inertia/pages/admin/index.tsx
@@ -1,11 +1,11 @@
 import PageTitle from '~/components/admin/common/page_title'
 import AdminLayout from '~/layout/admin'
 
-const AdminPostsPage = () => {
-  return <PageTitle>All Posts</PageTitle>
+const AdminHomePage = () => {
+  return <PageTitle>Dashboard Home</PageTitle>
 }
 
 // Persistent layout
-AdminPostsPage.layout = (page: any) => <AdminLayout children={page} />
+AdminHomePage.layout = (page: any) => <AdminLayout children={page} />
 
-export default AdminPostsPage
+export default AdminHomePage
